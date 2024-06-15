a60da0a5c3f9985d812cd0c50d9e0283bebed3a0
Mihir Sahu <123735395+mihirsahu36@users.noreply.github.com>
2024-06-15 17:55:27 +0530

    credentials.py

---
+++ credentials.py
@@ -0,0 +1,9 @@
+account_sid = "Twilio_account_sid"
+
+auth_token = "Twilio_auth_token"
+
+my_phone_number = "Your_phone_number"
+
+my_twilio_number = "Twilio_generated_number"
+
+owm_api_key = "Open_Weather_Map_API_Key"
